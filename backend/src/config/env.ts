37577d28a7af8ee71806e8bac5b3193d74047f4e
shakthivel <sakthivel2003al@gmail.com>
2026-06-29 06:01:00 +0530

    added centeralized configuration env.ts

---
+++ backend/src/config/env.ts
@@ -0,0 +1,22 @@
+// backend/src/config/env.ts
+
+/**
+ * Loads environment variables and exposes them in one place.
+ * Why?
+ * Instead of using process.env everywhere in the project,
+ * we centralize configuration here.
+ */
+
+// global imports 
+import dotenv from "dotenv"
+
+// load variables from .env into process.env
+dotenv.config()
+
+// Any environment variable used by the application should be exposed through this object.
+
+export const env = {
+   PORT: Number(process.env.PORT) || 5000,
+   NODE_ENV: process.env.NODE_ENV || "development",
+}
+
